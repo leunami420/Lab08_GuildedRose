004ffe3f753b2dad4b0454ba2d36488dea7d9a9c
leunami420 <130581919+leunami420@users.noreply.github.com>
2023-06-26 13:08:47 +0200

    Update gilded_rose.py

--- gilded_rose/refactored/gilded_rose.py
+++ gilded_rose/refactored/gilded_rose.py
@@ -1,39 +1,77 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
+        self.quality_update_strategies = {
+            "Aged Brie": AgedBrieUpdateStrategy(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassUpdateStrategy(),
+            "Sulfuras, Hand of Ragnaros": SulfurasUpdateStrategy(),
+            "Conjured": ConjuredUpdateStrategy()
+        }
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name in self.quality_update_strategies:
+                strategy = self.quality_update_strategies[item.name]
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                strategy = NormalItemUpdateStrategy()
+            strategy.update_quality(item)
+
+class QualityUpdateStrategy:
+    def update_quality(self, item):
+        item.sell_in -= 1
+
+class NormalItemUpdateStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        # Update quality logic for normal items
+        super().update_quality(item)
+        if (item.quality==0):
+            pass
+        elif(item.sell_in>0):
+            item.quality -= 1
+        elif (item.sell_in<0 and item.quality >= 2):
+            item.quality -= 2
+        else:
+            item.quality = 0
+
+class ConjuredUpdateStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        super().update_quality(item)
+        if (item.quality == 0):
+            pass
+        elif (item.sell_in > 0):
+            item.quality -= 2
+        elif (item.sell_in < 0 and item.quality >= 4):
+            item.quality -= 4
+        else:
+            item.quality = 0
+class AgedBrieUpdateStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        # Update quality logic for Aged Brie
+        super().update_quality(item)
+        if(item.quality<50):
+            item.quality += 1
+
+class BackstagePassUpdateStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        # Update quality logic for Backstage passes
+        super().update_quality(item)
+        if(item.sell_in<=0):
+            item.quality = 0
+        elif (item.sell_in <= 5 and item.quality<50):
+            item.quality += 3
+        elif (item.sell_in <= 10  and item.quality<50):
+            item.quality += 2
+        elif (item.sell_in > 10  and item.quality<50):
+            item.quality += 1
+
+
+class SulfurasUpdateStrategy(QualityUpdateStrategy):
+    def update_quality(self, item):
+        # Update quality logic for Backstage passes
+        pass
+
 
 
 class Item:
@@ -44,3 +82,4 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
